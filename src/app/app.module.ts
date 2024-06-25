b355dfc59c92f358248ea26be12de2383839784c
Tatiana Forero <tforerog@misena.edu.co>
2024-06-25 18:27:38 +0200

    frontend1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/components/empleados/empleados.component.ts
@@ -0,0 +1,32 @@
+import { Component,OnInit } from '@angular/core';
+import { EmpleadoService } from '../../services/empleado.service';
+import { NgForm } from '@angular/forms';
+import { Empleado } from '../../models/empleado';
+
+declare var M:any;
+
+@Component({
+  selector: 'app-empleados',
+  templateUrl: './empleados.component.html',
+  styleUrl: './empleados.component.css',
+  providers:[EmpleadoService]
+})
+export class EmpleadosComponent implements OnInit {
+  constructor(public empleadoService:EmpleadoService){}
+  ngOnInit(): void {
+      
+  }
+  agregarEmpleado(form?:NgForm){
+    this.empleadoService.postEmpleado(form?.value)
+    .subscribe(res =>{
+      this.resetForm(form);
+      M.toast({html:'Guardado correctamente'});
+    });
+  }
+  resetForm(form?:NgForm){
+    if(form){
+      form.reset();
+      this.empleadoService.selectedEmpleado = new Empleado();
+    }
+  }
+}
